4dd2333682d27a6e20f30310f9f344668d627d02
urstrulychetu007 <01fe19bcs132@kletech.ac.in>
2024-09-10 14:49:51 +0530

    Added spring data JPA to the Project

--- src/main/java/org/example/productservice/models/Product.java
+++ src/main/java/org/example/productservice/models/Product.java
@@ -1,10 +1,14 @@
 package org.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Product {
 
+    @Id
     private long id;
     private String name;
     private String description;
